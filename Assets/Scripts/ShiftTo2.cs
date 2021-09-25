53cb4c4dc79f136ea94ad0f945bd9e490de66f16
Aryamann Ningombam <aryamannsingh9@gmail.com>
2021-09-25 18:55:56 +0530

    first

---
+++ Assets/Scripts/ShiftTo2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ShiftTo2 : MonoBehaviour
+{
+ 
+
+ 
+    public void ChangeScene2()
+    {
+        print("Clicked");
+        SceneManager.LoadScene("Scene2");
+
+    }
+}
